8c56abd866d3c0196a6e00a4c515095d0f0656df
Carlos Yair Pacheco Tellez <carlosyairpachecotellez@192.168.1.11>
2026-07-28 12:13:58 -0600

    Realizar cambios en la estructura del login

--- views/panel_login.py
+++ views/panel_login.py
@@ -1,15 +1,34 @@
-from flet import Container, Column, Text, TextField, Colors, MainAxisAlignment, Alignment
+from flet import Container, Column, Text, TextField, FilledButton, Control, CrossAxisAlignment, FontWeight
 
 class PanelLogin(Container):
     def __init__(self) -> None:
         super() .__init__()
-        self.bgcolor = Colors.RED_400
-        self.alignment = Alignment.CENTER
+        self.inicializar_controles()
 
         self.content = Column(
-            controls=[
-                Text("Inicia sesion"),
-                TextField("Usuario"),
-                TextField("Contraseña"),
-            ]
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+            controls= self.arreglo_controles(),
         )
+
+    def inicializar_controles(self) -> None:
+        self.lbl_iniciar_sesion = Text("Inicia sesion",
+                                       weight=FontWeight.BOLD)
+        self.txt_ingresar_usuario = TextField(label="Usuario")
+        self.txt_ingrsar_contrasena = TextField(label="Contraseña")
+        self.btn_iniciar_sesion = FilledButton("Iniciar sesion")
+        self.lbl_mensaje_registrarse = Text("¿No tienes cuenta aun? Registrate")
+        self.btn_registrarse = FilledButton("Registrarse")
+
+    def arreglo_controles(self) -> list[Control]:
+        return[
+            self.lbl_iniciar_sesion,
+            self.txt_ingresar_usuario,
+            self.txt_ingrsar_contrasena,
+            self.btn_iniciar_sesion,
+            self.lbl_mensaje_registrarse,
+            self.btn_registrarse
+        ]
+    
+    def click_iniciar_sesion(self) -> None:
+        if self.txt_ingresar_usuario.value == "Carlos" and self.txt_ingrsar_contrasena.value == "12345":
+            pass
--- views/ventana.py
+++ views/ventana.py
@@ -1,9 +1,12 @@
-from flet import Page
+from flet import Page, MainAxisAlignment, CrossAxisAlignment, Colors
 from views.panel_login import PanelLogin
 
 class Ventana:
     def __init__(self, ventana: Page):
         self._ventana = ventana
+        self._ventana.bgcolor = "#EDEDE9"
+        self._ventana.vertical_alignment = MainAxisAlignment.CENTER
+        self._ventana.horizontal_alignment = CrossAxisAlignment.CENTER
         self._ventana.title = "UPT Delivery"
         self._ventana.add(PanelLogin())
         
